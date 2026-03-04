016ebbc890aa2bee49abe5ff627e8a9bc7c4630f
LAPTOP-AMQI5BOG\academia <202511_aw_toyoda@x-ide.sakura.ne.jp>
2026-03-04 12:26:49 +0900

    Spring Bootでフラッシュメッセージを実装

--- demo/src/main/java/com/example/demo/TodoController.java
+++ demo/src/main/java/com/example/demo/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/todo")
@@ -45,8 +46,13 @@ public class TodoController {
     }
 
     @PostMapping("/{id}/delete")
-    public String delete(@PathVariable("id") Long id) {
-        todoService.deleteById(id);
+    public String delete(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+        boolean deleted = todoService.deleteById(id);
+        if (deleted) {
+            redirectAttributes.addFlashAttribute("successMessage", "ToDoを削除しました");
+        } else {
+            redirectAttributes.addFlashAttribute("errorMessage", "削除に失敗しました");
+        }
         return "redirect:/todo";
     }
 }
